3e8bcfe1ec1c153e975d76ccbfedf89baf0777ce
sousousoudejin <2577552741@qq.com>
2023-05-09 21:57:05 +0800

    first commit.

---
+++ src/main/java/ltd/newbee/mall/controller/HelloController.java
@@ -0,0 +1,17 @@
+package ltd.newbee.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        System.out.println("输出Response");
+        return "hello! spring boot!!!";
+    }
+
+}
